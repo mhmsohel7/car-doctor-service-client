782f774653d186d3d65cef7e6d42b1c5f5a683c6
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-12-02 23:19:24 +0600

    initial set up - router set up - navbar - footer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-center uppercase">Sohel</h1>
+      <h1 className="text-6xl text-center uppercase">Sohel</h1>
+      <h1 className="text-6xl text-center uppercase">Sohel</h1>
+    </>
+  );
+}
+
+export default App;
