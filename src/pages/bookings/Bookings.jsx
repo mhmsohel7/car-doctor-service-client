2e05dfdf3527e076b1567db50b073f42a58cb12e
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-12-06 19:05:08 +0600

    delete method with set remaining & Update Method with status button

--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -13,7 +13,50 @@ const Bookings = () => {
       .then((data) => {
         setBookings(data);
       });
-  }, []);
+  }, [url]);
+
+  const handleDelete = (id) => {
+    const proceed = confirm("Are You sure you want to delete?");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+
+          if (data.deletedCount > 0) {
+            alert("Deleted Successfully");
+
+            //set Remaining
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          //   alert("");
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+        }
+      });
+  };
+
   return (
     <div>
       <h1 className="text-5xl">BOOKINGS : {bookings.length}</h1>
@@ -36,7 +79,12 @@ const Bookings = () => {
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <BookingsRows key={booking._id} booking={booking}></BookingsRows>
+              <BookingsRows
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+                handleBookingConfirm={handleBookingConfirm}
+              ></BookingsRows>
             ))}
           </tbody>
         </table>
